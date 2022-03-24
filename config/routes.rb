6cce3aaecf3111abf05ea451bc9f9b947d14a4ff
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-24 23:35:32 +0800

    Add nested route comments under articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # get 'articles', to: 'articles#index', as: 'articles'
   # post 'articles', to: 'articles#create'
   # get 'articles/new', to: 'articles#new', as: 'new_article'
