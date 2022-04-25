11e60e5feb24a233c58ee965e2a6b79704be7c6e
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-25 13:41:43 +0800

    Installation of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'articles/template_new', to: 'articles#template_new', as: 'template_new_articles'
   get 'articles/activities', to: 'articles#activities', as: 'article_activities'
