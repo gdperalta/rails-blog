6dc1b3b157c2b216ab24b7387030db8232a19ccf
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-27 14:13:16 +0800

    Add category and advertisement to created data Add ENV to create only chosen data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-roles = Role.create([
-                      { title: 'Author' },
-                      { title: 'Subscriber' }
-                    ])
+if ENV['roles']
+  roles = Role.create([
+                        { title: 'Author' },
+                        { title: 'Subscriber' }
+                      ])
+end
+
+if ENV['category']
+  category = Category.create([
+                               { name: 'busywork' },
+                               { name: 'charity' },
+                               { name: 'cooking' },
+                               { name: 'diy' },
+                               { name: 'education' },
+                               { name: 'music' },
+                               { name: 'recreational' },
+                               { name: 'relaxation' },
+                               { name: 'social' }
+                             ])
+end
+
+if ENV['advertisement']
+  advertisement = Advertisement.create([
+                                    { name: 'electronics' },
+                                    { name: 'jewelry' },
+                                    { name: "men's clothing" },
+                                    { name: "women's clothing" }
+                                  ])
+end
