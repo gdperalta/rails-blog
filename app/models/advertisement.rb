3846f3efb0ba1a8f76abb9b1c1a938c539de1c2a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-27 13:59:57 +0800

    Add ads method

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,4 +1,18 @@
 class Advertisement < ApplicationRecord
   has_many :article_advertisements
   has_many :articles, through: :article_advertisements
+
+  def ads
+    case name
+    when "men's clothing"
+      i = rand(1..4)
+    when 'jewelry'
+      i = rand(5..8)
+    when 'electronics'
+      i = rand(9..14)
+    when "women's clothing"
+      i = rand(15..20)
+    end
+    Product::Client.product(i)[:data]
+  end
 end
