a24b1a3e95be5d00c5689ab851c9257fc0777e3f
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-28 22:37:49 +0800

    Add instance variable to client class Add line that creates new class after Request.call

--- app/api/ActivityGenerator/client.rb
+++ app/api/ActivityGenerator/client.rb
@@ -1,11 +1,23 @@
 module ActivityGenerator
   class Client
+    attr_reader :activity, :type, :key, :accessibility, :response
+
+    def initialize(response)
+      @response = response
+      @activity = response[:data]['activity']
+      @type = response[:data]['type']
+      @key = response[:data]['key']
+      @accessibility = response[:data]['accessibility']
+    end
+
     def self.randomize
-      Request.call(http_method: 'get', endpoint: '/')
+      response = Request.call(http_method: 'get', endpoint: '/')
+      new(response)
     end
 
     def self.type(type)
-      Request.call(http_method: 'get', endpoint: "?type=#{type}")
+      response = Request.call(http_method: 'get', endpoint: "?type=#{type}")
+      new(response)
     end
   end
 end
--- app/api/Product/client.rb
+++ app/api/Product/client.rb
@@ -1,7 +1,16 @@
 module Product
   class Client
+    attr_reader :title, :image, :response
+
+    def initialize(response)
+      @response = response
+      @title = response[:data]['title']
+      @image = response[:data]['image']
+    end
+
     def self.product(product_id)
-      Request.call(http_method: 'get', endpoint: "/#{product_id}")
+      response = Request.call(endpoint: "/#{product_id}")
+      new(response)
     end
   end
 end
