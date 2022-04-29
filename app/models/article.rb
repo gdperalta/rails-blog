5e9ecd6cb2c2655f32240a4a2bc69a4d89d4cb0a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-29 14:47:57 +0800

    Add dependent: destroy to comments and article categories and advertisements Add allow_destroy and reject args to accepts_nested_attributes_for article_advertisements

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,12 +3,12 @@ class Article < ApplicationRecord
   scope :has_a_new_comment, -> { joins(:comments).where("comments.created_at > '2020-03-18'") }
 
   belongs_to :author
-  has_many :comments
-  has_many :article_advertisements
+  has_many :comments, dependent: :destroy
+  has_many :article_advertisements, dependent: :destroy
   has_many :advertisements, through: :article_advertisements
-  has_many :article_categories
+  has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories
-  accepts_nested_attributes_for :article_advertisements
+  accepts_nested_attributes_for :article_advertisements, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :article_categories
   validates :content, presence: true
   validates :title, presence: true, uniqueness: true
